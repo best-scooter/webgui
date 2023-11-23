6993835e2b4cb7b57f650098798951d6ef6a07fd
Despina-P <depl22@student.bth.se>
2023-11-23 15:59:03 +0100

    Added functions for main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Login from './components/Login';
+import Register from './components/Register';
+import AccountDetails from './components/AccountDetails';
 
-function App() {
+const App = () => {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const handleLogin = (username) => {
+    // Uppdatera tillståndet för inloggad användare
+    setLoggedInUser(username);
+  };
+
+  const handleRegister = (username) => {
+    // Implementera ytterligare logik om det behövs
+    setLoggedInUser(username);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+    {loggedInUser ? (
+      <AccountDetails username={loggedInUser} />
+    ) : isRegistering ? (
+      <Register onRegister={handleRegister} />
+    ) : (
+      <Login onLogin={handleLogin} onRegister={() => setIsRegistering(true)} />
+    )}
+  </div>
+);
+};
 
 export default App;
