092ef0887c773bba4dfc4b591d672ed7279384c3
Despina-P <depl22@student.bth.se>
2023-12-11 16:09:25 +0100

    Added first try of payment view

---
+++ src/components/Payments.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'; 
+//import { Link } from 'react-router-dom';
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+const Payments = ({ onLogin }) => {
+    return (
+        <p>Testar sidan payments...</p>
+    )
+}
+
+export default Payments;
