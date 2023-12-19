709f41d30f558a244bf84668c514596c8f20dda2
Despina-P <depl22@student.bth.se>
2023-12-19 13:04:44 +0100

    Added logic to add money to account

--- src/components/Payments.js
+++ src/components/Payments.js
@@ -1,10 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-//import { Link } from 'react-router-dom';
-//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const Payments = ({ onLogin }) => {
-  return <p>Testar sidan payments...</p>
-}
+const Payments = ({ oAuthToken, loggedInUser }) => {
+  const [amount, setAmount] = useState('');
 
-export default Payments
+  const handleDeposit = async () => {
+    try {
+      // PUT /customer/{customerId}
+      const response = await axios.put(
+        `http://localhost:1337/v1/customer/${loggedInUser}`,
+        {
+          balance: amount,
+        },
+        {
+          headers: {
+            'X-Access-Token': oAuthToken,
+          },
+        }
+      );
+
+      console.log('Deposit successful', response.data);
+    } catch (error) {
+      console.error('Error depositing funds:', error);
+    }
+  };
+
+  const handleMonthlyPayment = async () => {
+    // Implement logic for monthly payment here
+    // ...
+    console.log('Monthly payment successful');
+  };
+
+  return (
+    <div>
+      <h1>Payments</h1>
+      <label>
+        Deposit Amount:
+        <input
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+      </label>
+      <button onClick={handleDeposit}>Deposit</button>
+      <br />
+      <button onClick={handleMonthlyPayment}>Monthly Payment</button>
+    </div>
+  );
+};
+
+export default Payments;
+
