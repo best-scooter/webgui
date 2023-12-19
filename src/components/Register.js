248ba753d1020723ae066726763b21f1e52f8936
Despina-P <depl22@student.bth.se>
2023-12-19 13:05:08 +0100

    Added logic to register new user

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,37 +1,44 @@
-// Register.js
-import React, { useState } from 'react'
-import './Register.css' // Importera CSS-fil
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const Register = ({ onRegister }) => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+const Register = ({ onRegisterSuccess }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
 
-  const handleRegister = () => {
-    // Implementera här logik för registrering
-    // Exempel: Anropa en API för att skapa ett nytt konto
-    // och om det är framgångsrikt, anropa onRegister
-    onRegister(username)
-  }
+  const handleRegister = async () => {
+    try {
+      // POST /customer
+      const response = await axios.post('http://localhost:1337/v1/customer', {
+        name,
+        email,
+      });
+
+      // Anropa en callback-funktion för att meddela att registreringen var framgångsrik
+      if (onRegisterSuccess) {
+        onRegisterSuccess();
+      }
+
+      console.log('Registration successful', response.data);
+    } catch (error) {
+      console.error('Error registering user:', error);
+    }
+  };
 
   return (
-    <div className="container">
-      <div className="input-group">
-        <input
-          type="text"
-          placeholder="Användarnamn"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div className="input-group">
-        <input
-          type="password"
-          placeholder="Lösenord"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button onClick={handleRegister}>Registrera</button>
+    <div>
+      <h1>Register</h1>
+      <label>
+        Name:
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <button onClick={handleRegister}>Register</button>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
+
